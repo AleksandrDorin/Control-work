2ba425000bd45e54aafd325e3c41311661283be8
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-09-14 16:22:55 +0300

    void FillArray, PrintArray added

---
+++ Task/Program.cs
@@ -0,0 +1,22 @@
+﻿void FillArray(string[] array)
+{
+    Console.WriteLine("Enter the array values: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        string text = Console.ReadLine() ?? "0";
+        array[i] = text;
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine("Enter the size of the array: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+string[] array = new string[size];
+string[] copyarray = new string[size];
+FillArray(array);
+Console.WriteLine("An array is set, with values:");
+PrintArray(array);
