6ea70a5afd6fb1b1bb4a449f5de298f31a147193
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-09-14 16:33:34 +0300

    void ChangeArray added. Task completed

--- Task/Program.cs
+++ Task/Program.cs
@@ -13,6 +13,19 @@ void PrintArray(string[] array)
         Console.Write($"{array[i]} ");
 }
 
+void ChangeArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+}
+
 Console.WriteLine("Enter the size of the array: ");
 int size = int.Parse(Console.ReadLine() ?? "0");
 string[] array = new string[size];
@@ -20,3 +33,7 @@ string[] copyarray = new string[size];
 FillArray(array);
 Console.WriteLine("An array is set, with values:");
 PrintArray(array);
+Console.WriteLine("");
+ChangeArray(array, copyarray);
+Console.WriteLine("A new array with values whose length is less than or equal to 3 characters: ");
+PrintArray(copyarray);
